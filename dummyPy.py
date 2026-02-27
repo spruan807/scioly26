ca7168142a7aa3af90c176fa2766627b5c00a991
spruan807 <shruan807@gmail.com>
2026-02-27 15:27:42 +0000

    completed "drive" action in strategizer

--- dummyPy.py
+++ dummyPy.py
@@ -3,7 +3,7 @@ from easygopigo3 import EasyGoPiGo3
 import time
 import math
 
-# to do: make bot class
+# to do: separate classes
 
 ########### INITIALIZE ROBOT OBJECT ###########
 # create an instance of the GoPiGo3 class.
@@ -15,9 +15,9 @@ gpg = EasyGoPiGo3()
 ### IDEAL SPEED ###
 targetMin = 1 # minutes of target time
 targetSec = 5 # seconds of target time
-pathLength = 20.5 # total length of path, including back and forth
+# pathLength = 20.5 # total length of path, including back and forth
 
-nTurns = 9 # number of turns in path
+# nTurns = 9 # number of turns in path
 
 ### ONLY FOR REFERENCE ###
 sqLen = 50 # DO NOT CHANGE. the width of one square unit on the track.
@@ -84,17 +84,29 @@ class Drive():
         pass
 
 class Strat():
-    def __init__(self):
-        speed = self.calcIdealSpeed()
-        gpg.set_speed(speed) # initialize speed
-    
+    def __init__(self,sl,cl,el,sd,cd,ed,p):
+        pathLen = 0
+        totalR = 0
+        totalL = 0
+
+        startLoc = sl
+        currLoc = cl
+        endLoc = el
+
+        startDirh = sd
+        currFaceh = self.reverseDirh(cd)
+        endDirh = ed
+
+        steps = p
+
     def calcIdealSpeed(self):
+        nTurns = self.totalR + self.totalL
         totalTurnTime = nTurns * turnTime
         totalTime = targetMin * 60 + targetSec - totalTurnTime
 
         wheelCircumference = wheelDiameter * math.pi
         
-        totalSqs = pathLength * sqLen
+        totalSqs = self.pathLen * sqLen
         
         totalRotations = totalSqs / wheelCircumference
         totalDegrees = totalRotations * 360
@@ -102,46 +114,8 @@ class Strat():
         
         return math.ceil(ans)
     
-    def calcDirt(self, a,b):
-        # returns dir tuple
-        # a start
-        # b end
-        
-        x1,y1 = a
-        x2,y2 = b
-        
-        hor = x2 - x1
-        ver = y2 - y1
-        
-        unitHor = hor/abs(hor)
-        unitVer = ver/abs(ver)
-        
-        return (unitHor, unitVer)
-
-    def calcDists(self, a,b):
-        # returns taxicab distance
-        # a start
-        # b end
-        
-        x1,y1 = a
-        x2,y2 = b
-        
-        hor = x2 - x1
-        ver = y2 - y1
-        
-        return (hor,ver)
-
-    def calcHalfLoc(self, loc,dir):
-        # returns location tuple
-        lx,ly = loc
-        vx,vy = dirts[dir]
-        hx,hy = 0.5*vx, 0.5*vy
-        halfLoc = (lx+hx,ly+hy)
-
-        return halfLoc
-        
+    ### turn helpers ###
     
-
     def reverseDirt(self,t):
         x,y = t
         return (-1*x, -1*y)
@@ -150,72 +124,177 @@ class Strat():
         t = dirts[h]
         nt = self.reverseDirt(t)
         nh = dirhs[nt]
-
         return nh
 
-
-    def calcCW(self, t):
+    def dirtR(self, t):
         x,y = t
         x = (x+1)%2
         y = (y+1)%2
+        return (x,y) 
+
+    def dirtL(self, t):
+        x,y = t
+        x = (x-1)%2
+        y = (y-1)%2
         return (x,y)    
 
-    def chooseDirh(self, tr,tl):
+    def chooseDirh(self):
         ans = "r"
-        if tr>tl:
+        if self.totalR > self.totalL:
             ans = "l"
         return ans
 
-    def calcTurn(self, cd,fd,tr,tl):
-        cDirt = cx, cy = dirts[cd]
-        fDirt = fx, fy = dirts[fd]
-        currDirh = "r"
+    ### turn calculator ###
+    def calcNewDirt(self,goal):
+        # takes in goal loc
+        # returns dir tuple
+        # a start
+        # b end
+        
+        x1,y1 = self.currLoc
+        x2,y2 = goal
+        
+        hor = x2 - x1
+        ver = y2 - y1
+        
+        unitHor = hor/abs(hor)
+        unitVer = ver/abs(ver)
+        
+        return (unitHor, unitVer)
+    
+    def calcNTurnsh(self,nextDirh):
+        # returns direction, number of times to do
+        turnDirh = self.chooseDirh()
+        countTurns = 0
 
-        tx,ty = (fx-cx), (fy-cy)
+        fx,fy = f = dirts[self.faceCurrh]
+        nx,ny = n = dirts[nextDirh]
 
-        hflip = (cd=="l" or cd=="r") and (fd=="l" or fd=="r")
-        vflip = (cd=="u" or cd=="d") and (fd=="u" or fd=="d")
-        flip = hflip or vflip
+        dx,dy = (fx-nx), (fy-ny)
 
         # {"u":(0,-1), "d":(0,1), "l":(-1,0), "r":(1,0)}
-        # ur (1,-1)  r
-        # ul (-1,1)  l
-        # dr (1,1)   l
-        # dl (-1,-1) r
-        # ll (0,0)
+        # ur (1,1)  r
+        # ru (-1,-1) l
 
-        # if flip:
-        #     currDirh = chooseDirh(tr,tl)
+        # ul (-1,1)  l
+        # lu (1,-1)  r
+
+        # dr (1,-1)   l
+        # rd (-1,1) r
+
+        # dl (-1,-1) r
+        # ld (1,1) l
+
+        # ll (0,0)
+        # ud (0,2)
+        # du (0,-2)
+        # lr (2,0)
+        # rl (-2,0)
+
+        if abs(dx)==2 or abs(dy)==2:
+            countTurns = 2
+        elif f=="u" or n=="u":
+            if (dx==1 and turnDirh=="l") or (dx==-1 and turnDirh=="r"):
+                countTurns = 3
+        elif f=="d" or n=="d":
+            if (dx==1 and turnDirh=="r") or (dx==-1 and turnDirh=="l"):
+                countTurns = 3
             
-        pass
+        return (turnDirh, countTurns)
+
+    def updateCounters(self,ntd,nt,nd):
+        if ntd=="r": 
+            self.totalR+=nt
+        else:
+            self.totalL+=nt
+        
+        self.pathLen+=nd
+
+    def calcDriveSequenceh(self, goalLoc, goalDirh):
+        # returns code instruction tokens as list of code tuples
+        # (act,dir/dist,nTurns/0)
+        ans = []
+        horDist, verDist = dist = self.calcDist(goalLoc)
+        dirtx, dirty = dirt = self.calcNewDirt(goalLoc)
+
+        order = [((0,dirty), verDist), ((dirtx,0), horDist)]
+
+        if goalDirh=="v":
+            order = [((dirtx,0), horDist),((0,dirty), verDist)]
+
+        for (nextDirt, nextDist) in order:
+            newTurnDirh, nTurns = self.calcNTurnsh(nextDirt)
+
+            self.updateCounters(newTurnDirh,nTurns,nextDist) #this shouldn't be here
+
+            ans.append(("r",newTurnDirh,nTurns))
+            ans.append(("d",nextDist,0))
+
+        return ans
+
+
+    ### distance helpers ###
+    
+    def calcDist(self,goal):
+        # returns horizontal and vert distances
+        # a start
+        # b end
+        
+        x1,y1 = self.currLoc
+        x2,y2 = goal
+        
+        hor = x2 - x1
+        ver = y2 - y1
+        
+        return (hor,ver)
+
+    def calcEdgeLoc(self,loc,dir):
+        # returns location tuple of dir edge of loc square
+        lx,ly = loc
+        vx,vy = dirts[dir]
+        hx,hy = 0.5*vx, 0.5*vy
+        halfLoc = (lx+hx,ly+hy)
+
+        return halfLoc
+    
+    ### strategizer ###
+    def strategizer(self):
+        ans=[]
+        for step in self.steps:
+            act,loc,dirh = step
+
+            if act=="d":
+                newSteps = self.calcDriveSequenceh(loc,dirh)
+                ans = ans + newSteps
+    
 
 class Bobby:
-    def __init__(self, sl, cl, el, sd, cd, ed):
-        driver = Drive()
-        logicker = Strat()
+    def __init__(self, sl, cl, el, sd, cd, ed, p):
+        pathStr = p
+        path = self.parseInit()
 
-        startLoc = sl
-        currLoc = cl
-        endLoc = el
-        startDirh = sd
-        currDirh = logicker.reverseDir(cd)
-        endDirh = ed
+        logicker = Strat(sl,cl,el,sd,cd,ed,path) # init strat object
 
-        totalDist = 0
-        totalR = 0
-        totalL = 0
+        speed = logicker.calcIdealSpeed()
 
-    def parse(self,str):
+        driver = Drive(speed) # init drive object
+
+        gpg.set_speed(speed) # initialize speed
+
+    def parseInit(self):
         # shall return list of instructions
-        # example: "d-A3-l d-C3-u g-E1-u"
+        # example: "d-A3-h d-C3-v g-E1-v"
         # [("d", "A3", "l"), ("d", "C3", "u"), ("g", "E1", "u")]
         ans = []
-        insts = str.split()
+        insts = self.pathStr.split()
         for i in insts:
             parts = i.split("-")
             ans.append(tuple(parts))
 
         return ans
+    
+    def parseToken(self):
+        pass
     
     def run(self,str):
     # str is a string of instructions
@@ -228,20 +307,10 @@ class Bobby:
     # D0 D1 D2 D3 D4
     # E0 E1 E2 E3 E4
     #
-    # to drive to A3 from the left, type d-A3-l
+    # to drive to A3 from the horizontal side, type d-A3-h
     # to drive to the right edge of A4, type hd-A4-r
-    # to enter a gate at E1 from the top, g-E1-u
-    # no special water bottle function yet
+        pass
 
-        insts = self.parse(str)
-
-        for i in insts:
-            act, loc, dirh = i
-            dirt = dirts[dirh]
-
-            if act=="d":
-                # in progress
-                pass
 
 
 ########### WRITE CODE BELOW ###########
